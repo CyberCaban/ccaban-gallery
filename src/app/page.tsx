1d5c522b842de1c1868342d782ec6dcc8eeaf479
CyberCaban <andrewgamer63@gmail.com>
2024-05-09 21:19:07 +0300

    changed schema + design changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,28 @@
 import Image from "next/image";
 import { db } from "~/server/db";
 
-const urls = [
-  "https://utfs.io/f/dd165ae0-38e5-44f5-91ef-a513ab4c5b64-rb0ijz.jpg",
-  "https://utfs.io/f/533a6900-4438-4953-aaa7-22619be581e0-9mzwx1.jpg",
-  "https://utfs.io/f/f5db4b3b-aa49-424b-aaef-b9b46f4c9805-o9xd2t.jpg",
-  "https://utfs.io/f/f69944ea-0a29-48d3-bb66-5be762e136e9-shtgsn.jpg",
-];
-
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
-  console.log(posts);
+  console.log(images);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-4 ">
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 ">
-          {posts.map((image) => (
-            <div key={image.id} className="w-full p-4">
-              <span>{image.name}</span>
-            </div>
-          ))}
-          {[...urls, ...urls, ...urls].map((url, idx) => (
+          {[...images, ...images].map((image, idx) => (
             <div key={`${idx + 1}`} className="w-full p-4">
               <Image
                 width={1000}
                 height={1000}
-                src={url}
-                alt="CCaban_gallery"
+                src={image.url}
+                alt={image.name}
               />
+              <span>{image.name}</span>
             </div>
           ))}
         </div>
